6c615dd63f60aa3e9ad892561001f28aa3c25177
Fran9001 <franco.1998@outlook.com>
2024-06-13 23:46:48 -0300

    feat(about, contact, category): add

---
+++ src/components/pages/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="text-3xl text-stone-900">
+          ABOUT US
+        </div>
+    )
+
+}
+
+export default About;
---
+++ src/components/pages/category/storage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Storage = () => {
+    return (
+        <div className="text-3xl text-stone-900">
+          STORAGE
+        </div>
+    )
+}
+
+export default Storage
